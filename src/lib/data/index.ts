744cff6c2c9434f0ebb0669e9600f56e8307317c
hoangbao0201 <hoangbao020103@gmail.com>
2023-11-29 17:18:19 +0700

    Fix BE URL

--- src/lib/data/index.ts
+++ src/lib/data/index.ts
@@ -1,4 +1,5 @@
-export const API_BASE_URL = process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:4000";
+export const API_BASE_URL = process.env.NEXT_PUBLIC_BASE_URL ? process.env.NEXT_PUBLIC_BASE_URL : "http://localhost:4000";
+// export const API_BASE_URL = "https://hoangbaovn-be.onrender.com";
 
 export const DEFAULT_METADATA = {
     nameWeb: "HOANGBAOVN",
